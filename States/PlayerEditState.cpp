904cd83f1f2ac2f0a14118ade6f2b11a0a7763e5
alexeevdv <mail@alexeevdv.ru>
2012-11-28 16:50:39 +0400

    labels text changed

--- States/PlayerEditState.cpp
+++ States/PlayerEditState.cpp
@@ -13,31 +13,31 @@ PlayerEditState::PlayerEditState(Game * game) : State(game)
     
     _increaseStrenghtButton = new ImageButton("art/intrface/splsoff.frm","art/intrface/splson.frm",149,39);
     _decreaseStrenghtButton = new ImageButton("art/intrface/snegoff.frm","art/intrface/snegon.frm",149,50);
-    _statsStrenghtLabel = new TextArea(_t(306,"text/english/game/stat.msg"), 103,46);   
+    _statsStrenghtLabel = new TextArea(_t(200,"text/english/game/editor.msg"), 102,46);   
     
     _increasePerceptionButton = new ImageButton("art/intrface/splsoff.frm","art/intrface/splson.frm",149,39 + 33);
     _decreasePerceptionButton = new ImageButton("art/intrface/snegoff.frm","art/intrface/snegon.frm",149,50 + 33);
-    _statsPerceptionLabel = new TextArea(_t(307,"text/english/game/stat.msg"), 103,46 + 33);   
+    _statsPerceptionLabel = new TextArea(_t(201,"text/english/game/editor.msg"), 102,46 + 33);   
     
     _increaseEnduranceButton = new ImageButton("art/intrface/splsoff.frm","art/intrface/splson.frm",149,39 + 33*2);
     _decreaseEnduranceButton = new ImageButton("art/intrface/snegoff.frm","art/intrface/snegon.frm",149,50 + 33*2);
-    _statsEnduranceLabel = new TextArea(_t(308,"text/english/game/stat.msg"), 103,46 + 33*2);   
+    _statsEnduranceLabel = new TextArea(_t(202,"text/english/game/editor.msg"), 102,46 + 33*2);   
     
     _increaseCharismaButton = new ImageButton("art/intrface/splsoff.frm","art/intrface/splson.frm",149,39 + 33*3);
     _decreaseCharismaButton = new ImageButton("art/intrface/snegoff.frm","art/intrface/snegon.frm",149,50 + 33*3);
-    _statsCharismaLabel = new TextArea(_t(309,"text/english/game/stat.msg"), 103,46 + 33*3);   
+    _statsCharismaLabel = new TextArea(_t(203,"text/english/game/editor.msg"), 102,46 + 33*3);   
     
     _increaseIntelligenceButton = new ImageButton("art/intrface/splsoff.frm","art/intrface/splson.frm",149,39 + 33*4);
     _decreaseIntelligenceButton = new ImageButton("art/intrface/snegoff.frm","art/intrface/snegon.frm",149,50 + 33*4);
-    _statsIntelligenceLabel = new TextArea(_t(310,"text/english/game/stat.msg"), 103,46 + 33*4);   
+    _statsIntelligenceLabel = new TextArea(_t(204,"text/english/game/editor.msg"), 102,46 + 33*4);   
     
     _increaseAgilityButton = new ImageButton("art/intrface/splsoff.frm","art/intrface/splson.frm",149,39 + 33*5);
     _decreaseAgilityButton = new ImageButton("art/intrface/snegoff.frm","art/intrface/snegon.frm",149,50 + 33*5);
-    _statsAgilityLabel = new TextArea(_t(304,"text/english/game/stat.msg"), 103,46 + 33*5);   
+    _statsAgilityLabel = new TextArea(_t(205,"text/english/game/editor.msg"), 102,46 + 33*5);   
 
     _increaseLuckButton = new ImageButton("art/intrface/splsoff.frm","art/intrface/splson.frm",149,39 + 33*6);
     _decreaseLuckButton = new ImageButton("art/intrface/snegoff.frm","art/intrface/snegon.frm",149,50 + 33*6);
-    _statsLuckLabel = new TextArea(_t(305,"text/english/game/stat.msg"), 103,46 + 33*6);   
+    _statsLuckLabel = new TextArea(_t(206,"text/english/game/editor.msg"), 102,46 + 33*6);   
     
     add(background);
     add(_increaseStrenghtButton);
