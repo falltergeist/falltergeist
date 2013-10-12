6f9fde9fccd2cb5c904e3a78aea829ec0e846680
Dmitry V. Alexeev <mail@alexeevdv.ru>
2013-10-12 13:04:36 +0400

    Copyright info added

--- src/States/StartState.h
+++ src/States/StartState.h
@@ -15,6 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Falltergeist.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef FALLTERGEIST_STARTSTATE_H
