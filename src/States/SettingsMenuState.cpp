395f3dfe5eb031448315f9f4115d0041482fe2b0
Dmitry V. Alexeev <mail@alexeevdv.ru>
2013-10-27 21:49:45 +0400

    SettingsMenuState updated

--- src/States/SettingsMenuState.cpp
+++ src/States/SettingsMenuState.cpp
@@ -47,12 +47,12 @@ void SettingsMenuState::init()
 {    
     State::init();
     
-    
+    /*
     _fon = new InteractiveSurface();
     _fon->loadFromBmpFile(CrossPlatform::findDataPath() + "/scr00000.bmp");
     _fon->onKeyboardRelease((EventHandler) &SettingsMenuState::onButtonPress);
     add(_fon);
-    
+    */
     auto background = new Surface(ResourceManager::surface("art/intrface/prefscrn.frm", 0, 0));
     background->setXOffset(0);
     background->setYOffset(0);
@@ -63,17 +63,24 @@ void SettingsMenuState::init()
     auto switch4 = new MultistateImageButton(MultistateImageButton::TYPE_BIG_SWITCH, 76, 309);
     auto switch5 = new MultistateImageButton(MultistateImageButton::TYPE_BIG_SWITCH, 76, 387);
 
+    auto switch6 = new MultistateImageButton(MultistateImageButton::TYPE_SMALL_SWITCH, 299, 74);
+    auto switch7 = new MultistateImageButton(MultistateImageButton::TYPE_SMALL_SWITCH, 299, 74 + 66);
+    auto switch8 = new MultistateImageButton(MultistateImageButton::TYPE_SMALL_SWITCH, 299, 74 + 66*2);
+    auto switch9 = new MultistateImageButton(MultistateImageButton::TYPE_SMALL_SWITCH, 299, 74 + 66*3);
+    auto switch10 = new MultistateImageButton(MultistateImageButton::TYPE_SMALL_SWITCH, 299,74 + 66*4);
+    auto switch11 = new MultistateImageButton(MultistateImageButton::TYPE_SMALL_SWITCH, 299,74 + 66*5);
+
     // LABELS
     auto msg = ResourceManager::msgFileType("text/english/game/options.msg");
     
     auto title1 = new TextArea(msg->message(100), 74, 10);
     title1->setFont("font4.aaf")->setColor(0xffb89c28);
     
-    auto title2 = new TextArea(msg->message(101), 21, 48);
+    auto title2 = new TextArea(msg->message(102), 21, 48);
     title2->setColor(0xffb89c28)->setWidth(158)->setHorizontalAlign(TextArea::HORIZONTAL_ALIGN_CENTER)->setFont("font3.aaf");
 
     auto title3 = new TextArea(title2, 21, 48 + 77);
-    title2->setText(msg->message(102));
+    title2->setText(msg->message(101));
 
     auto title4 = new TextArea(title2, 21, 48 + 156);
     title4->setText(msg->message(103));
@@ -137,11 +144,49 @@ void SettingsMenuState::init()
 
     auto label1 = new TextArea(msg->message(203), 43, 81); label1->setColor(0xffb89c28);
     auto label2 = new TextArea(msg->message(204), 68, 67); label2->setColor(0xffb89c28);
-    auto label3 = new TextArea(msg->message(205), 123, 81); label3->setColor(0xffb89c28);
+    auto label3 = new TextArea(msg->message(205), 122, 81); label3->setColor(0xffb89c28);
 
     auto label4 = new TextArea(msg->message(206), 13, 159); label4->setColor(0xffb89c28);
     auto label5 = new TextArea(msg->message(207), 68, 145); label5->setColor(0xffb89c28);
-    auto label6 = new TextArea(msg->message(208), 123, 159); label6->setColor(0xffb89c28);
+    auto label6 = new TextArea(msg->message(208), 122, 159); label6->setColor(0xffb89c28);
+
+    auto label7 = new TextArea(msg->message(214), 56, 236); label7->setColor(0xffb89c28);
+    auto label8 = new TextArea(msg->message(215), 65, 222); label8->setColor(0xffb89c28);
+    auto label9 = new TextArea(msg->message(207), 122, 236); label9->setColor(0xffb89c28);
+    auto label10 = new TextArea(msg->message(216), 122, 257); label10->setColor(0xffb89c28)->setWidth(60);
+
+    auto label11 = new TextArea(msg->message(202), 42, 319); label11->setColor(0xffb89c28);
+    auto label12 = new TextArea(msg->message(201), 88, 305); label12->setColor(0xffb89c28);
+    auto label13 = new TextArea(msg->message(213), 122, 319); label13->setColor(0xffb89c28)->setWidth(70)->setWordWrap(true);
+
+    auto label14 = new TextArea(msg->message(202), 42, 397); label14->setColor(0xffb89c28);
+    auto label15 = new TextArea(msg->message(201), 88, 383); label15->setColor(0xffb89c28);
+
+    auto label16 = new TextArea(msg->message(211), 203, 69); label16->setColor(0xffb89c28);
+         label16->setColor(0xffb89c28)
+                 ->setHorizontalAlign(TextArea::HORIZONTAL_ALIGN_RIGHT)
+                 ->setWidth(100);
+    auto label17 = new TextArea(msg->message(212), 320, 69); label17->setColor(0xffb89c28);
+
+    auto label18 = new TextArea(label16, 203, 69 + 67);
+    label18->setText(msg->message(202));
+    auto label19 = new TextArea(msg->message(201), 320, 69 + 67); label19->setColor(0xffb89c28);
+
+    auto label20 = new TextArea(label16, 203, 69 + 67 + 66);
+    label20->setText(msg->message(202));
+    auto label21 = new TextArea(msg->message(201), 320, 69 + 67 + 66); label21->setColor(0xffb89c28);
+
+    auto label22 = new TextArea(label16, 203, 69 + 67 + 66 + 64);
+    label22->setText(msg->message(209));
+    auto label23 = new TextArea(msg->message(219), 320, 69 + 67 + 66 + 64); label23->setColor(0xffb89c28);
+
+    auto label24 = new TextArea(label16, 203, 69 + 67 + 66 + 66 + 65);
+    label24->setText(msg->message(202));
+    auto label25 = new TextArea(msg->message(201), 320, 69 + 66 + 67 + 66 + 65); label25->setColor(0xffb89c28);
+
+    auto label26 = new TextArea(label16, 203, 69 + 67 + 66 + 64 + 65 + 68);
+    label26->setText(msg->message(202));
+    auto label27 = new TextArea(msg->message(201), 320, 69 + 64 + 67 + 66 + 65 + 68); label27->setColor(0xffb89c28);
 
     // BUTTONS
 
@@ -185,19 +230,23 @@ void SettingsMenuState::init()
 {221}{}{Тихо}
 {222}{}{Громко}
 {223}{}{Ярче}
-{224}{}{Широко}
-
-     
+{224}{}{Широко}     
      */
+
+    add(background);
     add({ title1, title2, title3, title4, title5, title6,
           title7, title8, title9, title10, title11, title12, title13,
           title14, title15, title16, title17, title18, title19, title20,
           title21, title22, title23, button1, button2, button3,
-          label1, label2, label3, label4, label5, label6 });
-    add({switch1, switch2, switch3, switch4, switch5});
+          label1, label2, label3, label4, label5, label6, label7, label8,
+          label9, label10, label11, label12, label13, label14, label15,
+          label16, label17, label18, label19, label20, label21, label22, label23,
+          label24, label25, label26, label27
+    });
+    add({switch1, switch2, switch3, switch4, switch5,
+         switch6, switch7, switch8, switch9, switch10, switch11
+    });
 
-    //for (auto& label : _labels) add(label.second);
-    
 }
 
 void SettingsMenuState::think()
