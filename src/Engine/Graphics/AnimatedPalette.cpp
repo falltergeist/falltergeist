28110620b2d3bba43417eda79a815c317dba576e
iSage <isage.dna@gmail.com>
2014-09-29 15:28:50 +0400

    Init counters in header

--- src/Engine/Graphics/AnimatedPalette.cpp
+++ src/Engine/Graphics/AnimatedPalette.cpp
@@ -33,32 +33,18 @@ AnimatedPalette::AnimatedPalette()
 {
     _monitorsPalette = new unsigned int[5];
     _monitorsPalette[0] = 0x6B6B6FFF; _monitorsPalette[1] = 0x63677FFF; _monitorsPalette[2] = 0x576B8FFF; _monitorsPalette[3] = 0x0093A3FF; _monitorsPalette[4] = 0x6BBBFFFF;
-    _monitorsCounter = 0;
-    _monitorsTicks = 0;
 
     _slimePalette = new unsigned int[4];
     _slimePalette[0] = 0x006C00FF; _slimePalette[1] = 0x0B7307FF; _slimePalette[2] = 0x1B7B0FFF; _slimePalette[3] = 0x2B831BFF;
-    _slimeCounter = 0;
-    _slimeTicks = 0;
 
     _shorePalette = new unsigned int[6];
     _shorePalette[0] = 0x533F2BFF; _shorePalette[1] = 0x4B3B2BFF; _shorePalette[2] = 0x433727FF; _shorePalette[3] = 0x3F3327FF; _shorePalette[4] = 0x372F23FF; _shorePalette[5] = 0x332B23FF;
-    _shoreCounter = 0;
-    _shoreTicks = 0;
 
     _fireSlowPalette = new unsigned int[5];
     _fireSlowPalette[0] = 0xFF0000FF; _fireSlowPalette[1] = 0xD70000FF; _fireSlowPalette[2] = 0x932B0BFF; _fireSlowPalette[3] = 0xFF7700FF; _fireSlowPalette[4] = 0xFF3B00FF;
-    _fireSlowCounter = 0;
-    _fireSlowTicks = 0;
 
     _fireFastPalette = new unsigned int[5];
     _fireFastPalette[0] = 0x470000FF; _fireFastPalette[1] = 0x7B0000FF; _fireFastPalette[2] = 0xB30000FF; _fireFastPalette[3] = 0x7B0000FF; _fireFastPalette[4] = 0x470000FF;
-    _fireFastCounter = 0;
-    _fireFastTicks = 0;
-
-    _blinkingRed = -1;
-    _blinkingRedCounter = 0;
-    _blinkingRedTicks = 0;
 }
 
 AnimatedPalette::~AnimatedPalette()
