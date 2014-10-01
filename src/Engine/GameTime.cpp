5e13ffee80fe980914a9ad7dfc21a4fd9e2dbdba
alexeevdv <mail@alexeevdv.ru>
2014-10-01 20:47:25 +0400

    GameTime improvement

--- src/Engine/GameTime.cpp
+++ src/Engine/GameTime.cpp
@@ -85,7 +85,68 @@ void GameTime::_increaseHours()
 
 void GameTime::_increaseDay()
 {
+    _day++;
 
+    switch(_month)
+    {
+        case 1: // January
+        case 3: // March
+        case 5: // May
+        case 7: // July
+        case 8: // August
+        case 10: // October
+        case 12:// December
+            if (_day == 32)
+            {
+                _day = 1;
+                _increaseMonth();
+            }
+            break;
+        case 4: // April
+        case 6: // June
+        case 9: // September
+        case 11:// November
+            if (_day == 31)
+            {
+                _day = 1;
+                _increaseMonth();
+            }
+            break;
+        case 2: // February
+            // leap year
+            if(_year%4 == 0 && (_year%100 != 0 || _year%400 == 0))
+            {
+                if (_day == 30)
+                {
+                    _day = 1;
+                    _increaseMonth();
+                }
+            }
+            else
+            {
+                if (_day == 29)
+                {
+                    _day = 1;
+                    _increaseMonth();
+                }
+            }
+    }
+
+}
+
+void GameTime::_increaseMonth()
+{
+    _month++;
+    if (_month == 13)
+    {
+        _month = 1;
+        _increaseYear();
+    }
+}
+
+void GameTime::_increaseYear()
+{
+    _year++;
 }
 
 unsigned int GameTime::ticks()
