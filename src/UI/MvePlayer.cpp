bd20669a517f3cdbe9b974e6b5ce3e71260c98bb
Mikhail Paulyshka <me@mixaill.tk>
2014-11-05 23:50:05 +0300

    add clock_gettime() implementation for MSVC

--- src/UI/MvePlayer.cpp
+++ src/UI/MvePlayer.cpp
@@ -45,6 +45,64 @@ int clock_gettime(int clk_id, struct timespec *t){
     t->tv_nsec = nseconds;
     return 0;
 }
+#elif _MSC_VER
+//Based on http://stackoverflow.com/questions/5404277/
+#include <Windows.h>
+#define CLOCK_MONOTONIC 1
+
+LARGE_INTEGER getFILETIMEoffset()
+{
+    SYSTEMTIME s;
+    FILETIME f;
+    LARGE_INTEGER t;
+    s.wYear = 1970;
+    s.wMonth = 1;
+    s.wDay = 1;
+    s.wHour = 0;
+    s.wMinute = 0;
+    s.wSecond = 0;
+    s.wMilliseconds = 0;
+    SystemTimeToFileTime(&s, &f);
+    t.QuadPart = f.dwHighDateTime;
+    t.QuadPart <<= 32;
+    t.QuadPart |= f.dwLowDateTime;
+    return (t);
+}
+void clock_gettime(int X, struct timespec *ts)
+{
+    LARGE_INTEGER t;
+    FILETIME f;
+    double nanoseconds;
+    static LARGE_INTEGER offset;
+    static double frequencyToNanoseconds;
+    static int initialized = 0;
+    static BOOL usePerformanceCounter = 0;
+    if (!initialized) {
+        LARGE_INTEGER performanceFrequency;
+        initialized = 1;
+        usePerformanceCounter = QueryPerformanceFrequency(&performanceFrequency);
+        if (usePerformanceCounter) {
+            QueryPerformanceCounter(&offset);
+            frequencyToNanoseconds = (double)performanceFrequency.QuadPart / 1000.;
+        }
+        else {
+            offset = getFILETIMEoffset();
+            frequencyToNanoseconds = 0.010;
+        }
+    }
+    if (usePerformanceCounter) QueryPerformanceCounter(&t);
+    else {
+        GetSystemTimeAsFileTime(&f);
+        t.QuadPart = f.dwHighDateTime;
+        t.QuadPart <<= 32;
+        t.QuadPart |= f.dwLowDateTime;
+    }
+    t.QuadPart -= offset.QuadPart;
+    nanoseconds = (double)t.QuadPart / frequencyToNanoseconds;
+    t.QuadPart = nanoseconds;
+    ts->tv_sec = t.QuadPart / 1000000000;
+    ts->tv_nsec = t.QuadPart % 1000000000;
+}
 #endif
 
 namespace Falltergeist
