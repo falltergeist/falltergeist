d6c5f986d157d7282945e7aaa5bf09e2c8c5fee2
Rohit Nirmal <rohitnirmal9@gmail.com>
2014-11-05 20:11:14 -0600

    Swap "pressed" and "unpressed" images of TYPE_DIALOG_RED_BUTTON.
    This produces the correct visuals on the barter buttons in
    the dialog and barter states.

--- src/UI/ImageButton.cpp
+++ src/UI/ImageButton.cpp
@@ -128,8 +128,8 @@ ImageButton::ImageButton(unsigned int type, int x, int y) : ActiveUI(x, y)
             _textures.push_back(ResourceManager::texture("art/intrface/alarmin.frm"));
             break;
         case TYPE_DIALOG_RED_BUTTON:
-            _textures.push_back(ResourceManager::texture("art/intrface/di_rdbt1.frm"));
             _textures.push_back(ResourceManager::texture("art/intrface/di_rdbt2.frm"));
+            _textures.push_back(ResourceManager::texture("art/intrface/di_rdbt1.frm"));
             break;
         case TYPE_DIALOG_REVIEW_BUTTON:
             _textures.push_back(ResourceManager::texture("art/intrface/di_rest1.frm"));
