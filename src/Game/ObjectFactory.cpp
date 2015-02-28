95c4bb4e38a639a308e9d953abbd0a15b252eb7c
iSage <isage.dna@gmail.com>
2015-02-28 15:54:01 +0300

    Add default transparency value

--- src/Game/ObjectFactory.cpp
+++ src/Game/ObjectFactory.cpp
@@ -309,7 +309,7 @@ GameObject* GameObjectFactory::createObject(unsigned int PID)
     object->setFID(proto->FID());
     object->setCanWalkThru(proto->flags()&0x00000010);
 
-    object->setTrans(GameObject::TRANS_NONE);
+    object->setTrans(GameObject::TRANS_DEFAULT);
     if (proto->flags()&0x00004000) object->setTrans(GameObject::TRANS_RED);
     if (proto->flags()&0x00008000) object->setTrans(GameObject::TRANS_NONE);
     if (proto->flags()&0x00010000) object->setTrans(GameObject::TRANS_WALL);
