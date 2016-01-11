3626d96805e8ffa6f56553017e89bedc870c4e19
alexeevdv <mail@alexeevdv.ru>
2016-01-11 20:21:21 +0300

    Settings via INI file

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -53,57 +53,67 @@ Settings::~Settings()
 
 bool Settings::save()
 {
-    auto file = make_unique<Ini::File>();
+    CrossPlatform::createDirectory(CrossPlatform::getConfigPath());
+    std::string configFile = CrossPlatform::getConfigPath() + "/config.ini";
+    std::ofstream stream(configFile);
 
-    auto video = file->section("video");
+    Logger::info() << "Saving config to " << configFile << std::endl;
+
+    if (!stream)
+    {
+        Logger::warning() << "Cannot open config file at `" << configFile << "`;" << std::endl;
+        return false;
+    }
+
+    Ini::File file;
+
+    auto video = file.section("video");
     video->setPropertyInt("width", _screenWidth);
     video->setPropertyInt("height", _screenHeight);
     video->setPropertyInt("scale", _scale);
     video->setPropertyBool("fullscreen", _fullscreen);
 
-    auto audio = file->section("audio");
+    auto audio = file.section("audio");
     audio->setPropertyBool("enabled", _audioEnabled);
     audio->setPropertyDouble("master_volume", _masterVolume);
     audio->setPropertyDouble("music_volume", _musicVolume);
     audio->setPropertyDouble("voice_volume", _voiceVolume);
+    audio->setPropertyDouble("sfx_volume", _sfxVolume);
+    audio->setPropertyString("music_path", _musicPath);
+    audio->setPropertyInt("buffer_size", _audioBufferSize);
 
+    auto logger = file.section("logger");
+    logger->setPropertyString("level", _loggerLevel);
+    logger->setPropertyBool("colors", _loggerColors);
 
-    /*
-           << "-- audio"  << std::endl
-           << "master_volume = " << std::to_string(_masterVolume) << std::endl
-           << "music_volume = " <<  std::to_string(_musicVolume) << std::endl
-           << "voice_volume = " <<  std::to_string(_voiceVolume) << std::endl
-           << "sfx_volume = " <<    std::to_string(_sfxVolume)  << std::endl
-           << "music_path = \"" << _musicPath << "\"" << std::endl
-           << "audio_buffer_size = " << _audioBufferSize << std::endl
-           << "-- logger" << std::endl
-           << "logger_level = \"" << _loggerLevel << "\"" << std::endl
-           << "logger_colors = " << (_loggerColors ? "true" : "false") << std::endl
-           << "-- game" << std::endl
-           << "init_location = \"" << _initLocation << "\"" << std::endl
-           << "force_location = " << (_forceLocation ? "true" : "false") << std::endl
-           << "display_fps = " << (_displayFps ? "true" : "false") << std::endl
-           << "worldmap_fullscreen = " << (_worldMapFullscreen ? "true" : "false") << std::endl
-           << "display_mouse_position = " << (_displayMousePosition ? "true" : "false") << std::endl
-           << "-- preferences" << std::endl
-           << "brightness = "  << std::to_string(_brightness) << std::endl
-           << "game_difficulty = " << _gameDifficulty << std::endl
-           << "combat_difficulty = " << _combatDifficulty << std::endl
-           << "combat_looks = " << (_combatLooks ? "true" : "false") << std::endl
-           << "combat_messages = " << (_combatMessages ? "true" : "false") << std::endl
-           << "combat_taunts = " << (_combatTaunts ? "true" : "false") << std::endl
-           << "combat_speed = " << _combatSpeed << std::endl
-           << "item_highlight = " << (_itemHighlight ? "true" : "false") << std::endl
-           << "language_filter = " << (_languageFilter ? "true" : "false") << std::endl
-           << "mouse_sensitivity = " << std::to_string(_mouseSensitivity) << std::endl
-           << "player_speedup = " << (_playerSpeedup ? "true" : "false") << std::endl
-           << "running = " << (_running ? "true" : "false") << std::endl
-           << "subtitles = " << (_subtitles ? "true" : "false") << std::endl
-           << "target_highlight = " << (_targetHighlight ? "true" : "false") << std::endl
-           << "text_delay = " << std::to_string(_textDelay) << std::endl
-           << "violence_level = " << _violenceLevel << std::endl;
+    auto game = file.section("game");
+    game->setPropertyString("init_location", _initLocation);
+    game->setPropertyBool("force_location", _forceLocation);
+    game->setPropertyBool("display_fps", _displayFps);
+    game->setPropertyBool("worldmap_fullscreen", _worldMapFullscreen);
+    game->setPropertyBool("display_mouse_position", _displayMousePosition);
 
-    */
+    auto preferences = file.section("preferences");
+    preferences->setPropertyDouble("brightness", _brightness);
+    preferences->setPropertyInt("game_difficulty", _gameDifficulty);
+    preferences->setPropertyInt("combat_difficulty", _combatDifficulty);
+    preferences->setPropertyBool("combat_looks", _combatLooks);
+    preferences->setPropertyBool("combat_messages", _combatMessages);
+    preferences->setPropertyBool("combat_taunts", _combatTaunts);
+    preferences->setPropertyInt("combat_speed", _combatSpeed);
+    preferences->setPropertyBool("item_highlight", _itemHighlight);
+    preferences->setPropertyBool("language_filter", _languageFilter);
+    preferences->setPropertyDouble("mouse_sensitivity", _mouseSensitivity);
+    preferences->setPropertyBool("player_speedup", _playerSpeedup);
+    preferences->setPropertyBool("running", _running);
+    preferences->setPropertyBool("subtitles", _subtitles);
+    preferences->setPropertyBool("targe_highlight", _targetHighlight);
+    preferences->setPropertyDouble("text_delay", _textDelay);
+    preferences->setPropertyInt("violence_level", _violenceLevel);
+
+    Ini::Writer writer(file);
+    writer.write(stream);
+
     return true;
 }
 
@@ -133,7 +143,80 @@ bool Settings::forceLocation() const
 }
 
 bool Settings::load()
-{    
+{
+    std::string configFile = CrossPlatform::getConfigPath() + "/config.ini";
+    std::ifstream stream(configFile);
+
+    Logger::info() << "Loading config from " << configFile << std::endl;
+
+    if (!stream)
+    {
+        Logger::warning() << "Cannot open config file at `" << configFile << "`;" << std::endl;
+        return false;
+    }
+
+    Ini::Parser parser(stream);
+    auto file = parser.parse();
+
+    auto video = file->section("video");
+    if (video)
+    {
+        _screenWidth = video->propertyInt("width", _screenWidth);
+        _screenHeight = video->propertyInt("height", _screenHeight);
+        _scale = video->propertyInt("scale", _scale);
+        _fullscreen = video->propertyBool("fullscreen", _fullscreen);
+    }
+
+    auto audio = file->section("audio");
+    if (audio)
+    {
+        _audioEnabled = audio->propertyBool("enabled", _audioEnabled);
+        _masterVolume = audio->propertyDouble("master_volume", _masterVolume);
+        _musicVolume = audio->propertyDouble("music_volume", _musicVolume);
+        _voiceVolume = audio->propertyDouble("voice_volume", _voiceVolume);
+        _sfxVolume = audio->propertyDouble("sfx_volume", _sfxVolume);
+        _musicPath = audio->propertyString("music_path", _musicPath);
+        _audioBufferSize = audio->propertyInt("buffer_size", _audioBufferSize);
+    }
+
+    auto logger = file->section("logger");
+    if (logger)
+    {
+        _loggerLevel = logger->propertyString("level", _loggerLevel);
+        _loggerColors = logger->propertyBool("colors", _loggerColors);
+    }
+
+    auto game = file->section("game");
+    if (game)
+    {
+        _initLocation = game->propertyString("init_location", _initLocation);
+        _forceLocation = game->propertyBool("force_location", _forceLocation);
+        _displayFps = game->propertyBool("display_fps", _displayFps);
+        _worldMapFullscreen = game->propertyBool("worldmap_fullscreen", _worldMapFullscreen);
+        _displayMousePosition = game->propertyBool("display_mouse_position", _displayMousePosition);
+    }
+
+    auto preferences = file->section("preferences");
+    if (preferences)
+    {
+        _brightness = preferences->propertyDouble("brightness", _brightness);
+        _gameDifficulty = preferences->propertyInt("game_difficulty", _gameDifficulty);
+        _combatDifficulty = preferences->propertyInt("combat_difficulty", _combatDifficulty);
+        _combatLooks = preferences->propertyBool("combat_looks", _combatLooks);
+        _combatMessages = preferences->propertyBool("combat_messages", _combatMessages);
+        _combatTaunts = preferences->propertyBool("combat_taunts", _combatTaunts);
+        _combatSpeed = preferences->propertyInt("combat_speed", _combatSpeed);
+        _itemHighlight = preferences->propertyBool("item_highlight", _itemHighlight);
+        _languageFilter = preferences->propertyBool("language_filter", _languageFilter);
+        _mouseSensitivity = preferences->propertyDouble("mouse_sensitivity", _mouseSensitivity);
+        _playerSpeedup = preferences->propertyBool("player_speedup", _playerSpeedup);
+        _running = preferences->propertyBool("running", _running);
+        _subtitles = preferences->propertyBool("subtitles", _subtitles);
+        _targetHighlight = preferences->propertyBool("targe_highlight", _targetHighlight);
+        _textDelay = preferences->propertyDouble("text_delay", _textDelay);
+        _violenceLevel = preferences->propertyInt("violence_level", _violenceLevel);
+    }
+
     return true;
 }
 
