fc009de223ad731505691439100db38aa73b49cf
iSage <isage.dna@gmail.com>
2016-02-24 00:36:57 +0300

    Fix movies

--- src/Graphics/Movie.cpp
+++ src/Graphics/Movie.cpp
@@ -58,19 +58,30 @@ void Movie::loadFromSurface(SDL_Surface* surface)
 void Movie::render(int x, int y)
 {
     std::vector<glm::vec2> vertices;
+    std::vector<glm::vec2> UV;
+
+    int width = 640;
+    int height = 320;
 
     glm::vec2 vertex_up_left    = glm::vec2( (float)x, (float)y);
-    glm::vec2 vertex_up_right   = glm::vec2( (float)(x+_texture->textureWidth()), (float)y);
-    glm::vec2 vertex_down_right = glm::vec2( (float)(x+_texture->textureWidth()), (float)(y+_texture->textureHeight()));
-    glm::vec2 vertex_down_left  = glm::vec2( (float)x, (float)(y+_texture->textureHeight()));
+    glm::vec2 vertex_up_right   = glm::vec2( (float)(x+width), (float)y);
+    glm::vec2 vertex_down_right = glm::vec2( (float)(x+width), (float)(y+height));
+    glm::vec2 vertex_down_left  = glm::vec2( (float)x, (float)(y+height));
 
     vertices.push_back(vertex_up_left   );
     vertices.push_back(vertex_down_left );
     vertices.push_back(vertex_up_right  );
-
     vertices.push_back(vertex_down_right);
-    vertices.push_back(vertex_up_right);
-    vertices.push_back(vertex_down_left);
+
+    glm::vec2 uv_up_left    = glm::vec2( 0.0, 0.0 );
+    glm::vec2 uv_up_right   = glm::vec2( (float)_texture->width()/(float)_texture->textureWidth(), 0.0 );
+    glm::vec2 uv_down_right = glm::vec2( (float)_texture->width()/(float)_texture->textureWidth(), (float)_texture->height()/(float)_texture->textureHeight() );
+    glm::vec2 uv_down_left  = glm::vec2( 0.0, (float)_texture->height()/(float)_texture->textureHeight() );
+
+    UV.push_back(uv_up_left   );
+    UV.push_back(uv_down_left );
+    UV.push_back(uv_up_right  );
+    UV.push_back(uv_down_right);
 
     // TODO: different shader
 
@@ -97,6 +108,8 @@ void Movie::render(int x, int y)
 
 
     GL_CHECK(glBindBuffer(GL_ARRAY_BUFFER, Game::getInstance()->renderer()->getTVBO()));
+
+    GL_CHECK(glBufferData(GL_ARRAY_BUFFER, UV.size() * sizeof(glm::vec2), &UV[0], GL_DYNAMIC_DRAW));
 
     GL_CHECK(glVertexAttribPointer(ResourceManager::getInstance()->shader("sprite")->getAttrib("TexCoord"), 2, GL_FLOAT, GL_FALSE, 0, (void*)0 ));
 
