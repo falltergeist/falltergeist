85d68c39ba396717597750a3b28cab6018b59d8a
iSage <isage.dna@gmail.com>
2016-04-30 01:54:06 +0300

    Meh

--- travis/linux.install.sh
+++ travis/linux.install.sh
@@ -2,6 +2,8 @@
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 
 #clang 3.6
+curl -sSL "http://llvm.org/apt/llvm-snapshot.gpg.key" | sudo -E apt-key add -
+echo "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main" | sudo tee -a /etc/apt/sources.list > /dev/null
 #sudo add-apt-repository llvm-toolchain-precise-3.6 -y
 
 # sdl2
@@ -16,7 +18,7 @@ sudo apt-get install -qq libglm-dev
 sudo apt-get install -qq mesa-common-dev
 
 
-#if [ "$CXX" = "clang++" ]; then sudo apt-get install -qq clang-3.6; fi
-#if [ "$CXX" = "clang++" ]; then export CXX="clang++-3.6" CC="clang-3.6"; fi
+if [ "$CXX" = "clang++" ]; then sudo apt-get install -qq clang-3.6; fi
+if [ "$CXX" = "clang++" ]; then export CXX="clang++-3.6" CC="clang-3.6"; fi
 if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.9; fi
 if [ "$CXX" = "g++" ]; then export CXX="g++-4.9" CC="gcc-4.9"; fi
