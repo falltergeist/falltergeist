a10d50e304d204cfd497b139f4d09fac3859a9bb
alexeevdv <mail@alexeevdv.ru>
2016-12-24 23:56:24 +0300

    cmake find modules fix

--- cmake/modules/FindGLEW.cmake
+++ cmake/modules/FindGLEW.cmake
@@ -4,44 +4,43 @@
 # Once done this will define
 #
 # GLEW_FOUND
-# GLEW_INCLUDE_PATH
+# GLEW_INCLUDE_DIR
 # GLEW_LIBRARY
 #
-IF (WIN32)
-FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
-$ENV{PROGRAMFILES}/GLEW/include
-${PROJECT_SOURCE_DIR}/ext/glew/include
-DOC "The directory where GL/glew.h resides")
-FIND_LIBRARY( GLEW_LIBRARY
-NAMES glew GLEW glew32 glew32s
-PATHS
-$ENV{PROGRAMFILES}/GLEW/lib
-${PROJECT_SOURCE_DIR}/ext/glew/lib
-DOC "The GLEW library")
-ELSE (WIN32)
-FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
-/usr/include
-/usr/local/include
-/sw/include
-/opt/local/include
-DOC "The directory where GL/glew.h resides")
-FIND_LIBRARY( GLEW_LIBRARY
-NAMES GLEW glew
-PATHS
-/usr/lib64
-/usr/lib
-/usr/local/lib64
-/usr/local/lib
-/sw/lib
-/opt/local/lib
-DOC "The GLEW library")
-ENDIF (WIN32)
 
-IF (GLEW_INCLUDE_PATH)
-SET( GLEW_FOUND 1 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-ELSE (GLEW_INCLUDE_PATH)
-SET( GLEW_FOUND 0 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-ENDIF (GLEW_INCLUDE_PATH)
+FIND_PATH(GLEW_INCLUDE_DIR glew.h
+  HINTS
+  $ENV{GLEWDIR}
+  PATH_SUFFIXES include/GL include
+  PATHS
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /usr/local/include/GL
+  /usr/include/GL
+  /sw # Fink
+  /opt/local # DarwinPorts
+  /opt/csw # Blastwave
+  /opt
+  ${PROJECT_ROOT_DIR}/ext/glew
+)
+#MESSAGE("GLEW_INCLUDE_DIR is ${GLEW_INCLUDE_DIR}")
 
-MARK_AS_ADVANCED( GLEW_FOUND )
-################################################
+FIND_LIBRARY(GLEW_LIBRARY
+  NAMES GLEW
+  HINTS
+  $ENV{GLEWDIR}
+  PATH_SUFFIXES lib64 lib
+  PATHS
+  /sw
+  /opt/local
+  /opt/csw
+  /opt
+  ${PROJECT_ROOT_DIR}/ext/glew
+)
+#MESSAGE("GLEW_LIBRARY is ${GLEW_LIBRARY}")
+
+INCLUDE(FindPackageHandleStandardArgs)
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GLEW REQUIRED_VARS GLEW_LIBRARY GLEW_INCLUDE_DIR)
+
+
--- cmake/modules/FindGLM.cmake
+++ cmake/modules/FindGLM.cmake
@@ -14,40 +14,22 @@
 # - https://bitbucket.org/alfonse/gltut/src/28636298c1c0/glm-0.9.0.7/FindGLM.cmake
 #
 
-# Additional modules
+FIND_PATH(GLM_INCLUDE_DIR glm.hpp
+  HINTS
+  $ENV{GLMWDIR}
+  PATH_SUFFIXES include/glm include
+  PATHS
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /usr/local/include/glm
+  /usr/include/glm
+  /sw # Fink
+  /opt/local # DarwinPorts
+  /opt/csw # Blastwave
+  /opt
+  ${PROJECT_ROOT_DIR}/ext/glm
+)
+#MESSAGE("GLM_INCLUDE_DIR is ${GLM_INCLUDE_DIR}")
+
 include(FindPackageHandleStandardArgs)
-
-if (WIN32)
-	# Find include files
-	find_path(
-		GLM_INCLUDE_DIR
-		NAMES glm/glm.hpp
-		PATHS
-		$ENV{PROGRAMFILES}/include
-		${PROJECT_ROOT_DIR}/ext/GLM/include
-		${GLM_ROOT_DIR}/include
-		DOC "The directory where glm/glm.hpp resides")
-else()
-	# Find include files
-	find_path(
-		GLM_INCLUDE_DIR
-		NAMES glm/glm.hpp
-		PATHS
-		/usr/include
-		/usr/local/include
-		/sw/include
-		/opt/local/include
-		${GLM_ROOT_DIR}/include
-		DOC "The directory where glm/glm.hpp resides")
-endif()
-
-# Handle REQUIRD argument, define *_FOUND variable
-find_package_handle_standard_args(GLM DEFAULT_MSG GLM_INCLUDE_DIR)
-
-# Define GLM_INCLUDE_DIRS
-if (GLM_FOUND)
-	set(GLM_INCLUDE_DIRS ${GLM_INCLUDE_DIR})
-endif()
-
-# Hide some variables
-mark_as_advanced(GLM_INCLUDE_DIR)
+find_package_handle_standard_args(GLM REQUIRED_VARS GLM_INCLUDE_DIR)
