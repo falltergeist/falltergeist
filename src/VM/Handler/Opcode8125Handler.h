00bda11d5ea4f669e75c30cf451178a11aa69763
Michał Janiszewski <janisozaur@users.noreply.github.com>
2018-10-28 14:28:15 +0100

    Update Opcode8125Handler.h include guard

--- src/VM/Handler/Opcode8125Handler.h
+++ src/VM/Handler/Opcode8125Handler.h
@@ -17,8 +17,8 @@
  * along with Falltergeist.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef FALLTERGEIST_VM_HANDLER_OPCODE8113_H
-#define FALLTERGEIST_VM_HANDLER_OPCODE8113_H
+#ifndef FALLTERGEIST_VM_HANDLER_OPCODE8125_H
+#define FALLTERGEIST_VM_HANDLER_OPCODE8125_H
 
 // C++ standard includes
 
@@ -40,4 +40,4 @@ namespace Falltergeist {
         }
     }
 }
-#endif // FALLTERGEIST_VM_HANDLER_OPCODE8113_H
+#endif // FALLTERGEIST_VM_HANDLER_OPCODE8125_H
