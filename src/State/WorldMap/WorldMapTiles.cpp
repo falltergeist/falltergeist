c57430dbe0828fd6a576a528f58ce194edddb566
twboc <hubert.sobkow@gmail.com>
2019-10-19 13:58:39 +0200

    inRenderView method to show tiles simplifies (fully implemented only in full screen)

--- src/State/WorldMap/WorldMapTiles.cpp
+++ src/State/WorldMap/WorldMapTiles.cpp
@@ -42,6 +42,35 @@ namespace Falltergeist
             }
         };
 
+        void WorldMap::renderTiles()
+        {
+            signed int tileMinX; // start X coordinate of current tile on world map
+            signed int tileMinY; // start Y coordinate of current tile on world map
+            // NB: can be unsigned, but it compared with signed deltaX and deltaY, so...
+
+            // copy tiles to screen if needed
+            for (unsigned int y=0; y<tilesNumberY; y++)
+            {
+                for (unsigned int x=0; x<tilesNumberX; x++)
+                {
+                    _tiles->setCurrentImage(y*tilesNumberX+x);
+                    tileMinX = x*tileWidth;
+                    tileMinY = y*tileHeight;
+                    // checking if tile is visible on screenMap
+                    // checking current tile borders
+                    // either xmin or xmax SHOULD belongs to map area AND
+                    // either ymin or ymax SHOULD belongs to map area
+                    if(inRenderView('x', tileMinX) && inRenderView('y', tileMinY) )
+                    {
+                        _tiles->images().at(y*tilesNumberX+x)->setPosition(Point(x*tileWidth-deltaX + offsetX, y*tileHeight-deltaY + offsetY));
+                        _tiles->images().at(y*tilesNumberX+x)->render();
+                    }
+                }
+            }
+        }
+
+        //we can avoid using the checks if a part  of map is in the wie
+        //by creating a container class that can be clipped
         bool WorldMap::inRenderView(const char orientation, signed int worldTileMin){
             if (orientation == 'x')
             {
@@ -62,33 +91,6 @@ namespace Falltergeist
                 return  inYmin || inYmax;
             }
         };
-
-        void WorldMap::renderTiles()
-        {
-            signed int tileMinX; // start X coordinate of current tile on world map
-            signed int tileMinY; // start Y coordinate of current tile on world map
-            // NB: can be unsigned, but it compared with signed deltaX and deltaY, so...
-
-            // copy tiles to screen if needed
-            for (unsigned int y=0; y<tilesNumberY; y++)
-            {
-                for (unsigned int x=0; x<tilesNumberX; x++)
-                {
-                    _tiles->setCurrentImage(y*tilesNumberX+x);
-                    tileMinX = x*tileWidth;
-                    tileMinY = y*tileHeight;
-                    // checking if tile is visible on screenMap
-                    // checking current tile borders
-                    // either xmin or xmax SHOULD belongs to map area AND
-                    // either ymin or ymax SHOULD belongs to map area
-                    // if(inRenderView('x', tileMinX) && inRenderView('y', tileMinY) )
-                    // {
-                        _tiles->images().at(y*tilesNumberX+x)->setPosition(Point(x*tileWidth-deltaX + offsetX, y*tileHeight-deltaY + offsetY));
-                        _tiles->images().at(y*tilesNumberX+x)->render();
-                    // }
-                }
-            }
-        }
 
     }
 }
