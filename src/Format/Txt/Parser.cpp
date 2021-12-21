5b4b3224140c1a075248b005862cb8e38b550845
Dmitry V. Alekseev <mail@alexeevdv.ru>
2021-12-21 09:31:32 +0300

    Fix stuff

--- src/Format/Txt/Parser.cpp
+++ src/Format/Txt/Parser.cpp
@@ -29,12 +29,16 @@ namespace Falltergeist
 
             void Parser::rtrim(std::string& line)
             {
-                line.erase(std::find_if(line.rbegin(), line.rend(), [](int c) {return std::isspace(c);}), line.end())
+                line.erase(std::find_if(line.rbegin(), line.rend(), [](unsigned char c) {
+                    return !std::isspace(c);
+                }).base(), line.end());
             }
 
             void Parser::ltrim(std::string& line)
             {
-                line.erase(line.begin(), std::find_if(line.begin(), line.end(), [](int c) {return !std::isspace(c);}))
+                line.erase(line.begin(), std::find_if(line.begin(), line.end(), [](unsigned char c) {
+                    return !std::isspace(c);
+                }));
             }
 
             void Parser::toLower(std::string& line)
