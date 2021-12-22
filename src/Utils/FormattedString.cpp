f69a8c04b9e4cb0e73f2e09f8d164b9b7e1aba7b
alexeevdv <mail@alexeevdv.ru>
2021-12-22 22:05:37 +0300

    fix initializer

--- src/Utils/FormattedString.cpp
+++ src/Utils/FormattedString.cpp
@@ -5,7 +5,7 @@ namespace Falltergeist {
         FormattedString::FormattedString(const std::string format, ...) {
             int n = 0, size = 100;
             bool b = false;
-            va_list marker = nullptr;
+            va_list marker = {};
 
             while (!b) {
                 _actualString.resize(size);
